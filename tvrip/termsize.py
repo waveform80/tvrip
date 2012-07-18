b1a825108dcb4778313642ebc5040821c3d4b82e
Dave Hughes <dave@waveform.org.uk>
2012-07-18 09:42:39 +0000

    Linted termsize

--- tvrip/termsize.py
+++ tvrip/termsize.py
@@ -16,89 +16,86 @@
 # You should have received a copy of the GNU General Public License along with
 # tvrip.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Contains a cross-platform terminal size querying routine"""
+
+from __future__ import (
+    unicode_literals,
+    print_function,
+    absolute_import,
+    division,
+    )
+
 import sys
-import types
+import struct
 
 __all__ = ['terminal_size']
 
 if sys.platform.startswith('win'):
-    try:
-        import win32console
-        import win32api
-        hstdin, hstdout, hstderr = win32api.STD_INPUT_HANDLE, win32api.STD_OUTPUT_HANDLE, win32api.STD_ERROR_HANDLE
-    except ImportError:
-        hstdin, hstdout, hstderr = -10, -11, -12
-        try:
-            import ctypes
-            import struct
-        except ImportError:
-            # If neither ctypes (Python 2.5+) nor PyWin32 (extension) is
-            # available, simply default to 80x25
-            def query_console_size(handle):
-                return None
-        else:
-            # ctypes query_console_size() adapted from
-            # http://code.activestate.com/recipes/440694/
-            def query_console_size(handle):
-                h = ctypes.windll.kernel32.GetStdHandle(handle)
-                if h:
-                    buf = ctypes.create_string_buffer(22)
-                    if ctypes.windll.kernel32.GetConsoleScreenBufferInfo(h, buf):
-                        (
-                            bufx, bufy,
-                            curx, cury,
-                            wattr,
-                            left, top, right, bottom,
-                            maxx, maxy,
-                        ) = struct.unpack('hhhhHhhhhhh', buf.raw)
-                        return (right - left + 1, bottom - top + 1)
-                return None
-    else:
-        # PyWin32 query_console_size() adapted from 
-        # http://groups.google.com/group/comp.lang.python/msg/f0febe6a8de9666b
-        def query_console_size(handle):
-            try:
-                csb = win32console.GetStdHandle(handle)
-                csbi = csb.GetConsoleScreenBufferInfo()
-                size = csbi['Window']
-                return (size.Right - size.Left + 1, size.Bottom - size.Top + 1)
-            except:
-                return None
-    def default_console_size():
-        return (80, 25)
+    # ctypes query_console_size() adapted from
+    # http://code.activestate.com/recipes/440694/
+    import ctypes
+
+    def terminal_size():
+        "Returns the size (cols, rows) of the console"
+
+        def get_handle_size(handle):
+            "Subroutine for querying terminal size from std handle"
+            handle = ctypes.windll.kernel32.GetStdHandle(handle)
+            if handle:
+                buf = ctypes.create_string_buffer(22)
+                if ctypes.windll.kernel32.GetConsoleScreenBufferInfo(
+                        handle, buf):
+                    (left, top, right, bottom) = struct.unpack(
+                        str('hhhhHhhhhhh'), buf.raw)[5:9]
+                    return (right - left + 1, bottom - top + 1)
+            return None
+
+        stdin, stdout, stderr = -10, -11, -12
+        return (
+            get_handle_size(stderr) or
+            get_handle_size(stdout) or
+            get_handle_size(stdin) or
+            # Default
+            (80, 25)
+        )
+
 else:
     # POSIX query_console_size() adapted from
     # http://mail.python.org/pipermail/python-list/2006-February/365594.html
     # http://mail.python.org/pipermail/python-list/2000-May/033365.html
     import fcntl
     import termios
-    import struct
     import os
-    hstdin, hstdout, hstderr = 0, 1, 2
-    def query_console_size(handle):
-        try:
-            buf = fcntl.ioctl(handle, termios.TIOCGWINSZ, '12345678')
-            row, col, rpx, cpx = struct.unpack('hhhh', buf)
-            return (col, row)
-        except:
-            return None
-    def default_console_size():
-        fd = os.open(os.ctermid(), os.O_RDONLY)
-        try:
-            result = query_console_size(fd)
-        finally:
-            os.close(fd)
-        if result:
-            return result
-        try:
-            return (os.environ['COLUMNS'], os.environ['LINES'])
-        except:
-            return (80, 24)
 
-def terminal_size():
-    # Try stderr first as it's the least likely to be redirected
-    for handle in (hstderr, hstdout, hstdin):
-        result = query_console_size(handle)
-        if result:
-            return result
-    return default_console_size()
+    def terminal_size():
+        "Returns the size (cols, rows) of the console"
+
+        def get_handle_size(handle):
+            "Subroutine for querying terminal size from std handle"
+            try:
+                buf = fcntl.ioctl(handle, termios.TIOCGWINSZ, '12345678')
+                row, col = struct.unpack(str('hhhh'), buf)[0:2]
+                return (col, row)
+            except IOError:
+                return None
+
+        stdin, stdout, stderr = 0, 1, 2
+        # Try stderr first as it's the least likely to be redirected
+        result = (
+            get_handle_size(stderr) or
+            get_handle_size(stdout) or
+            get_handle_size(stdin)
+        )
+        if not result:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            try:
+                result = get_handle_size(fd)
+            finally:
+                os.close(fd)
+        if not result:
+            try:
+                result = (os.environ['COLUMNS'], os.environ['LINES'])
+            except KeyError:
+                # Default
+                result = (80, 24)
+        return result
