72404794615a613b569904a84c8b445ce23b8c9f
Dave Jones <dave@waveform.org.uk>
2024-07-21 15:10:11 +0100

    WIP -- API documentation
    Tidy up the database module

--- tvrip/database.py
+++ tvrip/database.py
@@ -5,7 +5,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-"""Implements the data model for the tvrip application's database"""
+"""
+Implements the data model for the tvrip application's database
+"""
 
 import os
 import tempfile
@@ -35,7 +37,9 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 
 
 class Episode(DeclarativeBase):
-    """Represents an episode of a season of a program"""
+    """
+    Represents an episode of a :class:`Season` of a :class:`Program`.
+    """
 
     __tablename__ = 'episodes'
     __table_args__ = (
@@ -60,7 +64,10 @@ class Episode(DeclarativeBase):
 
     @property
     def ripped(self):
-        """Indicates whether the episode has been ripped yet"""
+        """
+        Returns a :class:`bool` indicating whether the episode has been ripped
+        yet.
+        """
         return bool(self.disc_id)
 
     def __init__(self, season, number, name):
@@ -69,22 +76,22 @@ class Episode(DeclarativeBase):
         self.name = name
 
     def __repr__(self):
-        return "<Episode(%s, %d, %d, %s)>" % (
-            repr(self.season.program.name),
-            self.season.number,
-            self.number,
-            repr(self.name)
-        )
+        return (
+            f"<Episode({self.season.program.name!r}, "
+            f"{self.season.number!r}, {self.number!r}, {self.name!r})>")
 
 
 class Season(DeclarativeBase):
-    """Represents a season of a program"""
+    """
+    Represents a season of a :class:`Program`
+    """
 
     __tablename__ = 'seasons'
 
-    program_name = Column(Unicode(200),
-                          ForeignKey('programs.name', onupdate='cascade', ondelete='cascade'),
-                          primary_key=True)
+    program_name = Column(
+        Unicode(200),
+        ForeignKey('programs.name', onupdate='cascade', ondelete='cascade'),
+        primary_key=True)
     number = Column(Integer, CheckConstraint('number >= 0'), primary_key=True)
     episodes = relationship('Episode', backref='season', order_by=[Episode.number])
 
@@ -93,11 +100,13 @@ class Season(DeclarativeBase):
         self.number = number
 
     def __repr__(self):
-        return "<Season(%s, %d)>" % (repr(self.program.name), self.number)
+        return f"<Season({self.program.name!r}, {self.number!r})>"
 
 
 class Program(DeclarativeBase):
-    """Represents a program (i.e. a TV series)"""
+    """
+    Represents a program (i.e. a TV series)
+    """
 
     __tablename__ = 'programs'
 
@@ -108,18 +117,20 @@ class Program(DeclarativeBase):
         self.name = name
 
     def __repr__(self):
-        return "<Program(%s)>" % repr(self.name)
+        return f"<Program({self.name!r})>"
 
 
 class AudioLanguage(DeclarativeBase):
-    """Represents an audio language in the stored configuration"""
+    """
+    Represents an audio language in the stored :class:`Configuration`.
+    """
 
     __tablename__ = 'config_audio'
 
-    config_id = Column(Integer,
-                       ForeignKey('config.id', onupdate='cascade',
-                                  ondelete='cascade'), default=1,
-                       primary_key=True)
+    config_id = Column(
+        Integer,
+        ForeignKey('config.id', onupdate='cascade', ondelete='cascade'),
+        default=1, primary_key=True)
     lang = Column(Unicode(3), primary_key=True)
     config = relationship('Configuration', back_populates='audio_langs')
 
@@ -128,17 +139,20 @@ class AudioLanguage(DeclarativeBase):
         self.lang = lang
 
     def __repr__(self):
-        return "<AudioLanguage(%s)>" % repr(self.lang)
+        return f"<AudioLanguage({self.lang!r})>"
 
 
 class SubtitleLanguage(DeclarativeBase):
-    """Represents a subtitle language in the stored configuration"""
+    """
+    Represents a subtitle language in the stored :class:`Configuration`.
+    """
 
     __tablename__ = 'config_subtitles'
 
-    config_id = Column(Integer,
-                       ForeignKey('config.id', onupdate='cascade', ondelete='cascade'),
-                       default=1, primary_key=True)
+    config_id = Column(
+        Integer,
+        ForeignKey('config.id', onupdate='cascade', ondelete='cascade'),
+        default=1, primary_key=True)
     lang = Column(Unicode(3), primary_key=True)
     config = relationship('Configuration', back_populates='subtitle_langs')
 
@@ -147,17 +161,21 @@ class SubtitleLanguage(DeclarativeBase):
         self.lang = lang
 
     def __repr__(self):
-        return "<SubtitleLanguage(%s)>" % repr(self.lang)
+        return f"<SubtitleLanguage({self.lang!r})>"
 
 
 class ConfigPath(DeclarativeBase):
-    """Represents a path to an external utility in the stored configuration"""
+    """
+    Represents a path to an external utility in the stored
+    :class:`Configuration`.
+    """
 
     __tablename__ = 'config_paths'
 
-    config_id = Column(Integer,
-                       ForeignKey('config.id', onupdate='cascade', ondelete='cascade'),
-                       default=1, primary_key=True)
+    config_id = Column(
+        Integer,
+        ForeignKey('config.id', onupdate='cascade', ondelete='cascade'),
+        default=1, primary_key=True)
     name = Column(Unicode(100), primary_key=True)
     path = Column(Unicode(300), nullable=False)
 
@@ -167,11 +185,13 @@ class ConfigPath(DeclarativeBase):
         self.path = path
 
     def __repr__(self):
-        return "<ConfigPath(%s, %s)>" % (repr(self.name), repr(self.path))
+        return f"<ConfigPath({self.name!r}, {self.path!r})>"
 
 
 class Configuration(DeclarativeBase):
-    """Represents a stored configuration for the application"""
+    """
+    Represents a stored configuration for the application
+    """
 
     __tablename__ = 'config'
     __table_args__ = (
@@ -196,41 +216,49 @@ class Configuration(DeclarativeBase):
     _duration_max = Column('duration_max', Integer, nullable=False, default=50)
     program_name = Column(Unicode(200))
     season_number = Column(Integer)
-    subtitle_format = Column(Unicode(6),
-                             CheckConstraint("subtitle_format in ('none', 'vobsub', 'pgs', 'cc', 'any')"),
-                             nullable=False, default='none')
-    audio_mix = Column(Unicode(6),
-                       CheckConstraint("audio_mix in ('mono', 'stereo', 'dpl1', 'dpl2')"),
-                       nullable=False, default='dpl2')
-    decomb = Column(Unicode(4),
-                    CheckConstraint("decomb in ('off', 'on', 'auto')"),
-                    nullable=False, default='off')
+    subtitle_format = Column(
+        Unicode(6),
+        CheckConstraint("subtitle_format in ('none', 'vobsub', 'pgs', 'cc', 'any')"),
+        nullable=False, default='none')
+    audio_mix = Column(
+        Unicode(6),
+        CheckConstraint("audio_mix in ('mono', 'stereo', 'dpl1', 'dpl2')"),
+        nullable=False, default='dpl2')
+    decomb = Column(
+        Unicode(4),
+        CheckConstraint("decomb in ('off', 'on', 'auto')"),
+        nullable=False, default='off')
     audio_all = Column(Boolean, nullable=False, default=False)
     audio_langs = relationship('AudioLanguage', back_populates='config')
     subtitle_all = Column(Boolean, nullable=False, default=False)
     subtitle_default = Column(Boolean, nullable=False, default=False)
     subtitle_langs = relationship('SubtitleLanguage', back_populates='config')
-    video_style = Column(Unicode(10),
-                         CheckConstraint("video_style in ('tv', 'film', 'animation')"),
-                         nullable=False, default='tv')
+    video_style = Column(
+        Unicode(10),
+        CheckConstraint("video_style in ('tv', 'film', 'animation')"),
+        nullable=False, default='tv')
     dvdnav = Column(Boolean, nullable=False, default=True)
-    duplicates = Column(Unicode(5),
-                        CheckConstraint("duplicates in ('all', 'first', 'last')"),
-                        nullable=False, default='all')
+    duplicates = Column(
+        Unicode(5),
+        CheckConstraint("duplicates in ('all', 'first', 'last')"),
+        nullable=False, default='all')
     api_key = Column(Unicode(128), nullable=False, default='')
-    api_url = Column(Unicode(300), nullable=False, default='https://api.thetvdb.com/')
-    output_format = Column(Unicode(3),
-                           CheckConstraint("output_format in ('mp4', 'mkv')"),
-                           nullable=False, default='mp4')
+    api_url = Column(
+        Unicode(300), nullable=False, default='https://api.thetvdb.com/')
+    output_format = Column(
+        Unicode(3),
+        CheckConstraint("output_format in ('mp4', 'mkv')"),
+        nullable=False, default='mp4')
     width_max = Column(Integer, nullable=False, default=1920)
     height_max = Column(Integer, nullable=False, default=1080)
     paths = relationship('ConfigPath', backref='config')
     program = relationship('Program')
-    season = relationship('Season',
-                          primaryjoin='and_('
-                          'Season.program_name == Configuration.program_name, '
-                          'Season.number == foreign(Configuration.season_number)'
-                          ')')
+    season = relationship(
+        'Season',
+        primaryjoin='and_('
+            'Season.program_name == Configuration.program_name, '
+            'Season.number == foreign(Configuration.season_number)'
+        ')')
 
     def _get_duration_min(self):
         return timedelta(minutes=self._duration_min)
@@ -238,8 +266,9 @@ class Configuration(DeclarativeBase):
     def _set_duration_min(self, value):
         self._duration_min = value.seconds / 60
 
-    duration_min = synonym('_duration_min',
-                           descriptor=property(_get_duration_min, _set_duration_min))
+    duration_min = synonym(
+        '_duration_min',
+        descriptor=property(_get_duration_min, _set_duration_min))
 
     def _get_duration_max(self):
         return timedelta(minutes=self._duration_max)
@@ -247,30 +276,39 @@ class Configuration(DeclarativeBase):
     def _set_duration_max(self, value):
         self._duration_max = value.seconds / 60
 
-    duration_max = synonym('_duration_max',
-                           descriptor=property(_get_duration_max, _set_duration_max))
+    duration_max = synonym(
+        '_duration_max',
+        descriptor=property(_get_duration_max, _set_duration_max))
 
     @property
     def max_resolution(self):
         return (self.width_max, self.height_max)
 
     def in_audio_langs(self, lang):
-        """Returns True if lang is a selected audio language"""
+        """
+        Returns :data:`True` if lang is a selected :class:`AudioLanguage`.
+        """
         return any(l.lang == lang for l in self.audio_langs)
 
     def in_subtitle_langs(self, lang):
-        """Returns True if lang is a selected subtitle language"""
+        """
+        Returns :data:`True` if lang is a selected :class:`SubtitleLanguage`.
+        """
         return any(l.lang == lang for l in self.subtitle_langs)
 
     def get_path(self, name):
-        """Returns the configured path of the specified utility"""
+        """
+        Returns the configured path of the specified utility.
+        """
         session = Session.object_session(self)
         return session.query(ConfigPath).\
             filter(ConfigPath.config_id == self.id).\
             filter(ConfigPath.name == name).one().path
 
     def set_path(self, name, value):
-        """Sets the configured path of the specified utility"""
+        """
+        Sets the configured path of the specified utility.
+        """
         session = Session.object_session(self)
         session.query(ConfigPath).\
             filter(ConfigPath.config_id == self.id).\
@@ -281,9 +319,9 @@ class Configuration(DeclarativeBase):
         return "<Configuration(...)>"
 
 
-def init_session(url='sqlite:///{DATADIR}/tvrip.db'.format(DATADIR=DATADIR),
-                 debug=False):
-    """Initializes the connection to the database and returns a new session
+def init_session(url=f'sqlite:///{DATADIR}/tvrip.db', debug=False):
+    """
+    Initializes the connection to the database and returns a new session
 
     This routine must be called once during the application to open the
     connection to the tvrip database and obtain a session object for
